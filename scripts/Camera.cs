e87417425b50eb93f1a0e2a7f2e90f3303c58477
alikhan khamidulla <khamidulla.alikhan@gmail.com>
2025-04-15 10:27:52 +0500

    ThirdView

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -9,26 +9,73 @@ public class Camera : MonoBehaviour
 	public GameObject player;
 	float xRotation, yRotation;
 	private GameObject weapon;
+	private bool isThirdView;
+	public Transform thirdViewPosition, firstViewPostition;
+	private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
-    {
+	{
+		if(!PlayerPrefs.HasKey("View"))
+		{
+			PlayerPrefs.SetInt("View", 1);
+		}
+		if(PlayerPrefs.GetInt("View") == 3)
+		{
+			isThirdView = true;
+			transform.position = thirdViewPosition.position;
+		}
+		else
+		{
+			isThirdView = false;
+			transform.position = firstViewPostition.position;
+		}
+		
 	    UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-	    Cursor.visible = false;
+		Cursor.visible = false;
+		if(weapon == null)
+		{
+			weapon = GameObject.FindGameObjectWithTag("Weapon");
+		}
+		
+		offset = new Vector3(-2,2, -5);
 
     }
 
     // Update is called once per frame
     void Update()
 	{
-		if(weapon == null)
+		
+		if(!isThirdView)
 		{
-			weapon = GameObject.FindGameObjectWithTag("Weapon");
+			CameraFirstViewMotion();
 		}
-	    CameraMotion();
+		else
+		{
+			CameraThirdViewMotion();
+		}
+		
+		if(Input.GetKeyDown(KeyCode.C))
+		{
+			if(isThirdView == false)
+			{
+				isThirdView = true;
+				PlayerPrefs.SetInt("View", 3);
+				transform.position = thirdViewPosition.position;
+
+			}
+			else
+			{
+				isThirdView = false;
+				PlayerPrefs.SetInt("View", 1);
+				transform.position = firstViewPostition.position;
+
+			}
+			
+		}
     }
     
-	public void CameraMotion()
+	public void CameraFirstViewMotion()
 	{
 		float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
 		float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensX;
@@ -44,6 +91,23 @@ public class Camera : MonoBehaviour
 		{
 			weapon.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
 		}
+	}
+	
+	public void CameraThirdViewMotion()
+	{
+		float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+		float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensX;
+		
+		yRotation += mouseX;
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -30f, 10f);
+		
+		Quaternion rotation = Quaternion.Euler(xRotation, yRotation, 0);
+		Vector3 direction = rotation * Vector3.back;
+
+		transform.position = player.transform.position + direction * Mathf.Abs(offset.z) + Vector3.up * offset.y;
+		transform.rotation = rotation;
+		player.transform.rotation = Quaternion.Euler(0, yRotation, 0);
 	}
 
 }
---
+++ scripts/test/ShaderTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ShaderTest : MonoBehaviour
+{
+	public Material lightsaberMaterial;
+	public float speed = 2.0f;
+
+	void Update()
+	{
+		if (lightsaberMaterial)
+		{
+			lightsaberMaterial.SetFloat("_ColorShiftSpeed", speed);
+		}
+	}
+}
